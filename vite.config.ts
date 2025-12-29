4f5022201000ef4ec2aa50c1b12ebb31a45a3402
bitch9bitch9 <zero0956001@gmail.com>
2025-12-30 03:22:50 +0800

    vercel兼容測試

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Citadel-ts/',
+export default defineConfig(() => {
+  // 檢查是否是在 GitHub Actions 環境下執行
+  // GitHub Actions 預設會有 GITHUB_ACTIONS 這個環境變數
+  const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
+  return {
+    plugins: [react()],
+    base: isGitHubPages ? '/Citadel-ts/' : '/', 
+  }
 })
